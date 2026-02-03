df9d8b23eb850834cafedd5b59bdcbe71a41b158
Jaguardu34 <lucas.reute@outlook.fr>
2026-02-03 16:29:19 +0100

    Third

--- info_products.js
+++ info_products.js
@@ -2,3 +2,7 @@
 const params = new URLSearchParams(window.location.search);
 const nom = params.get("nom");
 
+var buttonRetour = document.getElementById("buttonRetour");
+buttonRetour.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
